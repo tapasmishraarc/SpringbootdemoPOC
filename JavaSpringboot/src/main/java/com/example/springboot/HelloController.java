a552e1056451200823a78fa9c8309494a6eaec41
shivanshaarna12 <shivanshaarna12@outlook.com>
2021-01-11 11:00:57 +0000

    Updated HelloController.java

--- JavaSpringboot/src/main/java/com/example/springboot/HelloController.java
+++ JavaSpringboot/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Hello from Spring Boot!";
+		return "Hello from Spring Boot work item transition!";
 	}
 
 }
